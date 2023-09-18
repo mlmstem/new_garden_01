5986d30a6ea8ce79e1ac76e79eb0e1c426b0770b
LoveLive1987 <2962439864@qq.com>
2023-09-18 21:30:02 +1000

    send plant to database

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DragAndDrop : MonoBehaviour
-{
+{   
+    [SerializeField] private string plantServer = "http://127.0.0.1:13756/account/add-plant";
     Vector3 mousePosition;
     private bool onField = false;
 
@@ -52,10 +54,14 @@ public class DragAndDrop : MonoBehaviour
                 {
                     // Debug.Log("field is empty");
                     // allocate object to the field
+                    // Using post request here to send plant data to mongodb atlas
+
                     onField = true;
                     fieldStatus.setFull();
                     this.gameObject.transform.parent = col.gameObject.transform;
                     this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+
+                    StartCoroutine(SendPlantDataToServer());
                 }
                 else
                 {
@@ -70,4 +76,75 @@ public class DragAndDrop : MonoBehaviour
             }
         }
     }
+
+    IEnumerator SendPlantDataToServer()
+{
+    // Create a JSON object with the default plant data using JsonUtility
+    PlantData plantData = new PlantData
+    {
+        plantType = "tomato",
+        startDate = System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+        age = 0,
+        position = "unknown",
+        status = "Unknown",
+        moisturePercentage = 100,
+        temperatureCelsius = 100,
+        atmosphericPressurePa = 100
+    };
+
+    // Replace "YOUR_USERNAME" with the actual username of the user
+    string username = PlayerPrefs.GetString("Username", "DefaultUsername");
+
+    // Create a request object
+    UnityWebRequest request = UnityWebRequest.PostWwwForm(plantServer, "POST");
+    request.SetRequestHeader("Content-Type", "application/json");
+
+    // Create a JSON object to send in the request
+    PlantRequestData requestData = new PlantRequestData
+    {
+        username = username,
+        plantInfo = plantData
+    };
+
+    string requestBody = JsonUtility.ToJson(requestData);
+
+    byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(requestBody);
+    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+    // Send the POST request
+    yield return request.SendWebRequest();
+
+    if (request.result == UnityWebRequest.Result.Success)
+    {
+        // Plant added successfully
+        Debug.Log("Plant added successfully");
+    }
+    else
+    {
+        // Error handling if the request fails
+        Debug.LogError("Error sending plant data: " + request.error);
+    }
+    }
+
+    [System.Serializable]
+    public class PlantRequestData
+    {
+        public string username;
+        public PlantData plantInfo;
+    }
+
+    [System.Serializable]
+    public class PlantData
+    {
+        public string plantType;
+        public string startDate;
+        public int age;
+        public string position;
+        public string status;
+        public int moisturePercentage;
+        public int temperatureCelsius;
+        public int atmosphericPressurePa;
+    }
+
 }
--- Backend/route/authenticationRoutes.js
+++ Backend/route/authenticationRoutes.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 const Account = mongoose.model('accounts');
+const Plant = mongoose.model('Plant')
+const bodyParser = require('body-parser');
+
 
 module.exports = app =>{
+    app.use(bodyParser.json());
     app.get('/account', async(req, res) => {
     // use req.query to get what user inputing into the browser
     // use res (response) to send the information back to the browser
     
         const{rUsername, rPassword} = req.query;
 
-         if (rUsername == null || rPassword == null){
+         if (rUsername == null || rPassword == null)
+         {
             res.send("Invalid credentials");
             return;
-     }
+        }
+        console.log("Received request with parameters:", req.query);
+        //console.log("Sending response:", res);
+
 
         var userAccount = await Account.findOne({username : rUsername});
         if (userAccount == null){
@@ -21,7 +29,9 @@ module.exports = app =>{
                 username : rUsername,
                 password : rPassword,
 
-                lastAuthentication : Date.now()
+                lastAuthentication : Date.now(),
+                plantList: []
+
         });
 
             await newAccount.save();
@@ -32,11 +42,63 @@ module.exports = app =>{
 
             if(rPassword == userAccount.password){
                 userAccount.lastAuthentication = Date.now();
+                res.send(userAccount);
                 await userAccount.save();
 
                 return;
         }
      }
+     res.send("Invalid Credentials");
+     return;
 
     });
+
+    app.post('/account/add-plant', async (req, res) => {
+        try {
+            const { username, plantInfo } = req.body;
+    
+            if (!username || !plantInfo) {
+                res.status(400).json({ error: "Invalid request" });
+                return;
+            }
+    
+            // Find the user by username and add the new plant to their plantList
+            const userAccount = await Account.findOne({ username });
+    
+            if (!userAccount) {
+                res.status(404).json({ error: "User not found" });
+                return;
+            }
+            console.log("Request Body:", req.body)
+    
+            // Create a new Plant object and assign its properties
+            const newPlant = new Plant({
+                plantType: plantInfo.plantType,
+                startDate: plantInfo.startDate,
+                age: plantInfo.age,
+                position: plantInfo.position,
+                status: plantInfo.status,
+                moisturePercentage: plantInfo.moisturePercentage,
+                temperatureCelsius: plantInfo.temperatureCelsius,
+                atmosphericPressurePa: plantInfo.atmosphericPressurePa,
+            });
+    
+            // Save the new plant to the user's plantList
+            console.log("User Account:", userAccount);
+            console.log("New Plant Object:", newPlant);
+
+            userAccount.plantList.push(newPlant);
+            await userAccount.save();
+    
+            res.status(200).json({ message: "Plant added successfully", plant: newPlant });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    });
+
+    // Additional routes for handling plants...
+
+
+
 }
