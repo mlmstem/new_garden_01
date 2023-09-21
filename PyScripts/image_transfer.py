5e38d7bd39f2f9b81f1e38ff77467b0b63a9b39b
yeukhonjohnn <yeukhonjohnn@student.unimelb.edu.au>
2023-09-22 05:21:23 +1000

    created single entry point for all scripts, convert data analysis from local storage to database

--- PyScripts/image_transfer.py
+++ PyScripts/image_transfer.py
@@ -14,6 +14,10 @@ password_escaped = quote_plus(password)
 uri = f"mongodb+srv://{username_escaped}:{password_escaped}@cluster0.g9kdlqh.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+# Connect to the database and collection
+db = client['Plant']
+fs = gridfs.GridFS(db, collection="data_graphs")
+
 
 # Store the file
 def store_file(file_name):
@@ -32,11 +36,10 @@ def store_file(file_name):
 def retrieve_file(file_name):
     
     file = fs.find_one({"filename": file_name})
-    print(file._id)
     if file:
         with open('PyScripts/DB_Graphs/'+file_name, 'wb') as f:
             f.write(file.read())
-        print("File retrieved and saved as png!")
+        print("File retrieved!")
     else:
         print("File not found!")
 
@@ -47,7 +50,7 @@ def transfer_one(filename):
 
 #Transfer all files
 def transfer_all():
-    graph_dir_path = r'PyScripts\Graphs'
+    graph_dir_path = 'PyScripts\\Graphs'
     graph_list = []
 
     for file_path in os.listdir(graph_dir_path):
@@ -60,37 +63,7 @@ def transfer_all():
         store_file(x)
         retrieve_file(x)
 
-
-
-# Connect to the database and collection
-db = client['Plant']
-fs = gridfs.GridFS(db, collection="testpic")
-
-arguments = sys.argv[1:]
-if (len(arguments)!=1):
-    print("Arguments incorrect.")
-else:
-    if(arguments[0]=="all"):
-        print("Transferring all")
-        transfer_all()
-    elif(fnmatch.fnmatch(arguments[0], '*.png')):
-
-        graph_dir_path = r'PyScripts\Graphs'
-        graph_list = []
-
-        for file_path in os.listdir(graph_dir_path):
-            # check if current file_path is a file
-            if os.path.isfile(os.path.join(graph_dir_path, file_path)):
-                # add filename to list
-                graph_list.append(file_path)
-        if(arguments[0] not in graph_list):
-            print("File is not in folder.")
-        else:
-            print("Transferring "+arguments[0])
-            transfer_one(arguments[0])
-
-    else:
-        print("Arguments incorrect.")
-
-
-client.close()
+def download_all():
+    files = fs.list()
+    for x in files:
+        retrieve_file(str(x))
